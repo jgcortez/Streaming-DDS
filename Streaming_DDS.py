8b5614e38f85185b3008b01337e3579e9d428fb9
jgcortez <josuecortezlg@gmail.com>
2023-11-19 13:47:44 -0700

    Create MongoDG streaming_dds database, insert and query data

--- Streaming_DDS.py
+++ Streaming_DDS.py
@@ -1,4 +1,5 @@
 import json
+from pymongo import MongoClient
 
 # Function to load JSON files
 def load_file(file_path):
@@ -106,3 +107,48 @@ def delete(collection, query, delete_many=False):
         collection.delete_many(query)
     else:
         collection.delete_one(query)
+
+
+try:
+    # Connect to MongoDB
+    client = MongoClient('localhost', 27017)
+    db = client['streaming_dds']
+
+    # Create (Insert initial data) 
+    # (!!: Comment out after first run to avoid inserting duplicates)
+    create(db.playlists, playlists['playlists'])
+    create(db.search_queries, search_queries)
+    create(db.streaming_history, streaming_history)
+
+    # Verify Connection and Data Insertion
+    print("\nCollections in spotify_clone_db:", db.list_collection_names())
+    print("Number of documents in playlists:", db.playlists.count_documents({}))
+    print("Number of documents in search_queries:", db.search_queries.count_documents({}))
+    print("Number of documents in streaming_history:", db.streaming_history.count_documents({}))
+
+    # Read (Query)
+    print("\nSample Playlist Document:\n", list(read(db.playlists)))
+    print("\nSample Search Query Document:\n", list(read(db.search_queries)))
+    print("\nSample Streaming History Document:\n", list(read(db.streaming_history)))
+
+    # Other sample queries
+    print("\nFirst Document in Playlist Collection:\n", list(read(db.playlists, limit=1))) # Fetching the first document found in the playlists collection
+    print("\nPlaylists with the name \"promise\":\n", list(read(db.playlists, {"name": "promise"}, limit=1))) # Fetching playlists with a specific name
+    print("\nFirst 5 docuents from Search Queries Collection:\n", list(read(db.search_queries, limit=5))) # Fetching the first 5 documents from the search_queries collection
+    # print(list(read(db.streaming_history, {"$or": [{"track.trackName": "Scary Love"}, {"track.artistName": "The Neighbourhood"}]}, limit=5))) # Fetching streaming history records for a specific track or artist
+
+    # Update data (Do not use yet, to be used later)
+    # update(db.playlists, {'name': 'promise'}, {'$set': {'numberOfFollowers': 10}})
+    # update(db.search_queries, {}, {'$set': {'new_field': 'value'}}, update_many=True)
+
+    # Delete data (Do not use yet, to be used later)
+    # delete(db.streaming_history, {'track.trackName': 'Scary Love'})
+    
+except Exception as e:
+    import traceback
+    print("An error occurred: ", e)
+    traceback.print_exc()
+finally:
+    # Close the connection
+    client.close()
+
