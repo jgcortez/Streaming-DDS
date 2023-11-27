1db84a37f0d079b543c0ef77e56478e5241796ae
sanjithpk <sanjithpk06@gmail.com>
2023-11-27 10:25:30 -0700

    updating Part 2 and Part 3

--- Part 3/part3.py
+++ Part 3/part3.py
@@ -1,27 +1,25 @@
 from pymongo import MongoClient
 import time
 
-client = MongoClient('localhost', 27017)
+# Connect to the MongoDB cluster
+client = MongoClient('mongodb://localhost:27017/')
 db = client['streaming_dds']
-playlists_collection = db.playlists
+playlists_collection = db['playlists']
 
-# Function to measure query execution time in milliseconds
-def measure_query_time(query):
+# Function to measure query execution time
+def measure_query_time(collection, query):
     start_time = time.time()
-    list(playlists_collection.find(query))  # Execute the query and convert to list
+    list(collection.find(query))  # Execute the query
     end_time = time.time()
-    return (end_time - start_time) * 1000  # Convert seconds to milliseconds
+    return (end_time - start_time) * 1000  # Convert to milliseconds
 
-# Measure time before indexing
-time_before_indexing = measure_query_time({"name": {"$regex": "^S"}})
+# Measure query time before indexing
+query = {"name": "Sample Playlist Name"}  # Replace with your query
+time_before_indexing = measure_query_time(playlists_collection, query)
 print(f"Time before indexing: {time_before_indexing:.2f} milliseconds")
 
-# Creating an index on the 'name' field
 playlists_collection.create_index([('name', 1)])
 
-# Measure time after indexing
-time_after_indexing = measure_query_time({"name": {"$regex": "^S"}})
+# Measure query time after indexing
+time_after_indexing = measure_query_time(playlists_collection, query)
 print(f"Time after indexing: {time_after_indexing:.2f} milliseconds")
-
-# Drop the index after testing (optional)
-playlists_collection.drop_index([('name', 1)])
